ee10b07ea8b7c118936666ec2bb07568a2b0c3e0
Nico Finkernagel <nico@gruselhaus.com>
2019-04-15 17:17:15 +0200

    minor edits

--- src/utils/verify.test.js
+++ src/utils/verify.test.js
@@ -4,15 +4,14 @@
  */
 
 const verifyKey = require('./verify').verifyKey;
-const CI_KEY = process.env.CI_KEY;
 
 it('Testing with CI_KEY', async done => {
-  const expected = await verifyKey('monitoring', CI_KEY);
+  const expected = await verifyKey(process.env.SERVICE, process.env.CI_KEY);
   expect(expected).toBe(true);
   done();
 })
 it("Testing with key=''", async done => {
-  const expected = await verifyKey('monitoring', '');
+  const expected = await verifyKey(process.env.SERVICE, '');
   expect(expected).toBe(false);
   done();
 })
