89e908c9e8d683dbbff2b75bddae5f81d410318c
Nico Finkernagel <nico@gruselhaus.com>
2019-04-15 17:25:19 +0200

    Update URL

--- src/routes/github.js
+++ src/routes/github.js
@@ -7,7 +7,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.redirect("https://github.com/gruselhaus/monitoring.gruselhaus.com");
+  res.redirect("https://github.com/gruselhaus/api");
 });
 
 module.exports = router;
