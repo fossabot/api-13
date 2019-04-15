26dca7fbb9ba9373249b74bc86d537d227b940ff
Nico Finkernagel <nico@gruselhaus.com>
2019-04-15 18:44:20 +0200

    Update Firebase Auth Data

--- src/utils/verify.js
+++ src/utils/verify.js
@@ -8,11 +8,11 @@ require('dotenv').config();
 const firebase = require("firebase");
 const config = {
   apiKey: process.env.FIREBASE_KEY,
-  authDomain: "monitoring-gruselhaus-com.firebaseapp.com",
-  databaseURL: "https://monitoring-gruselhaus-com.firebaseio.com",
-  projectId: "monitoring-gruselhaus-com",
-  storageBucket: "monitoring-gruselhaus-com.appspot.com",
-  messagingSenderId: "952728646343"
+  authDomain: "verify-8a73f.firebaseapp.com",
+  databaseURL: "https://verify-8a73f.firebaseio.com",
+  projectId: "verify-8a73f",
+  storageBucket: "verify-8a73f.appspot.com",
+  messagingSenderId: "612769697074"
 };
 firebase.initializeApp(config);
 const db = firebase.database();
@@ -31,8 +31,8 @@ module.exports.verifyKey = (_service, _key) => {
       } else {
         resolve(false);
       }
-    }, () => {
-      reject(new Error('API Key Checking Failed!'));
+    }, (error) => {
+      reject(new Error(`API Key Checking Failed! ${error}`));
     });
   });
 }
