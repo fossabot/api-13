b7470cde19777e91d5f99533c89de262637a5288
Nico Finkernagel <nico@gruselhaus.com>
2019-04-16 05:54:27 +0200

    Add /key endpoint

---
+++ src/routes/getKeys.js
@@ -0,0 +1,35 @@
+/**
+ * @name status.js
+ * @author Nico Finkernagel
+ */
+
+const express = require('express');
+const router = express.Router();
+const db = require('../utils/database');
+const {verifyKey} = require("../utils/verify");
+const service = process.env.SERVICE;
+
+router.get("/:key", async (req, res) => {
+  const api_key = req.params.key;
+  if ((await verifyKey(service, api_key))) {
+    const ref = db.ref('/');
+    ref.on("value", (snapshot) => {
+      const resp = snapshot.val();
+      res.status(200).json(resp);
+    })
+  } else {
+    res.status(401).json({
+      status: 401,
+      error: [{
+        code: 401,
+        source: {
+          pointer: req.headers.host + "/" + req.url
+        },
+        title: "Authentication failed",
+        detail: "Did you used your API Key?"
+      }]
+    });
+  }
+});
+
+module.exports = router;
