eef9ad0f83db120ca56c1947b7eec06160db2e81
Julia Konstanz <julia@gruselhaus.com>
2019-09-07 23:17:35 +0200

    Removing unecessary round brackets

--- src/routes/db/database.ts
+++ src/routes/db/database.ts
@@ -1,16 +1,16 @@
 /*
  * File: database.ts
  * Project: gruselhaus-api
- * File Created: Tuesday, 6th August 2019 8:02:58 am
+ * File Created: Tuesday, 20th August 2019 9:48:57 pm
  * Author: Nico Finkernagel <nico@gruselhaus.com>
  * -----
- * Last Modified: Tuesday, 20th August 2019 8:01:29 pm
- * Modified By: Julia Konstanz <julia@gruselhaus.com>
+ * Last Modified: Saturday, 7th September 2019 11:17:05 pm
+ * Modified By: Nico Finkernagel <nico@gruselhaus.com>
  * -----
  * Copyright 2019 Nico Finkernagel <nico@gruselhaus.com>, all rights reserved.
  */
 
-import("../../env");
+import "../../env";
 const { Pool } = require("pg");
 
 export const pool = new Pool({
